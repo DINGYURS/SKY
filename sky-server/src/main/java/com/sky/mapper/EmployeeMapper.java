c652fd82e8dc2bae3e827eec1ddcfc086124a0a3
DINGYU <DINGYURS@qq.com>
2024-07-17 16:00:32 +0800

    编辑员工信息

--- sky-server/src/main/java/com/sky/mapper/EmployeeMapper.java
+++ sky-server/src/main/java/com/sky/mapper/EmployeeMapper.java
@@ -38,4 +38,8 @@ public interface EmployeeMapper {
      * @param employee
      */
     void update(Employee employee);
+
+    @Select("select id, name, username, password, phone, sex, id_number, status, create_time, update_time, create_user, update_user " +
+            "from employee where id = #{id}")
+    Employee getById(Long id);
 }
